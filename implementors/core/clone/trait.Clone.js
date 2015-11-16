4f7dd7acaaf714ba77e9c03b773642ef82c5d3a4
adjivas <adjivas@users.noreply.github.com>
2015-11-16 01:18:07 +0100

    update documentation

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['sem'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='sem/ffi/enum.Ipc.html' title='sem::ffi::Ipc'>Ipc</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='sem/ffi/enum.Sem.html' title='sem::ffi::Sem'>Sem</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
